275a44f67621020094440acbabb9fa078a076b99
zeroes-Saiajin <maxdem4uk@yandex.ru>
2018-09-13 22:05:36 +0300

    проверка

--- Demchuk/Max.py
+++ Demchuk/Max.py
@@ -1,1 +1,21 @@
-print("Hello sanya")
+class People:
+    def __init__(self,first_name,second_name,sex,age): #конструктор
+        self.first_name = first_name
+        self.second_name = second_name
+        self.age = age
+        self.sex = sex
+    def __str__(self):
+        return 'people <%s %s, age: %d, sex: %s>' % (self.first_name, self.last_name, self.age, self.sex)
+
+class Robots:
+    def Robot(part, chassis, body, specific): #конструктор
+        part.chassis = shassis
+        part.body = body
+        part.specific = specific
+    def __str__(part):
+        return 'robot <chassis: %s, body: %s, specific = %s' % (part.chassis, part.body, part.specific)
+
+class Circlerob:
+    def (self):
+        
+
